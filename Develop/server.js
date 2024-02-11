da85b695d8318c2c56f7aa148260125d87f5cbf2
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-02-10 17:29:16 -0800

    Add API route for POST new note

--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,69 @@
 const express = require('express');
 const path = require('path'); //for working with file paths
+const fs = require('fs');
+const util = require('util');
+const uuid = require('uuid');
 
 const app = express();  //create an Express application
+app.use(express.json()); //body parser middleware
 
 app.get('/notes', (req, res) => {
     const notesFilePath = path.join(__dirname, 'public', 'notes.html');
     res.sendFile(notesFilePath);
 });
 
-app.get('*', (req, res) => {
-    const indexFilePath = path.join(__dirname, 'public', 'index.html');
-    res.sendFile(indexFilePath);
-})
-
 app.get('/api/notes', (req, res) => {
     //read the db.json file an return all saved notes as JSON
-})
+    fs.readFile('./db/db.json','utf8',(err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data)
+            res.send(parsedNotes);
+        }
+    });
+});
 
 app.post('/api/notes', (req, res) => {
     // receive a new note to save on the request body, add it to the db.json file, 
     //and then return the new note to the client. You'll need to find a way to give 
     //each note a unique id when it's saved (look into npm packages that could do this for you).
+    console.info(`${req.method} request received to add a note`);   // Log that a POST request was received
+    const {title, text} = req.body;
+    const newNote = {
+        id: uuid.v4(),
+        title: title,
+        text: text
+    }
+
+    if(!newNote.title || !newNote.text){
+        return res.status(400).json('Please include note title and text.')
+    }
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data); // Convert string into JSON object
+            parsedNotes.push(newNote);            // Add the new note
+    
+            // Write updated notes back to the file
+            fs.writeFile('./db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+                writeErr
+                ? console.error(writeErr)
+                : res.send(newNote), console.info('Successfully updated notes!')
+            );
+        }
+    });
+})
+
+app.get('*', (req, res) => {
+    const indexFilePath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(indexFilePath);
 })
 
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Note Taker Starter Code
 
 Used https://www.youtube.com/watch?v=L72fhGm1tfE
+
+
+app.get('/api/notes') section copied from functionality in the Module 11 ex 28 mini-project
+
+POST request similar to class exersize 20 module 11
