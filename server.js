3af3e5e5b6245825147057750d98680038cf2ca2
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2024-02-21 15:28:37 -0800

    Change db/db.json file path references to troubleshoot Render

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 //GET notes API path
 app.get('/api/notes', (req, res) => {
     //read the db.json file an return all saved notes as JSON
-    fs.readFile('./db/db.json','utf8',(err, data) => {
+    fs.readFile('db/db.json','utf8',(err, data) => {
         if (err) {
             console.error(err);
         } else {
@@ -45,7 +45,7 @@ app.post('/api/notes', (req, res) => {
     }
 
 // Obtain existing notes
-fs.readFile('./db/db.json', 'utf8', (err, data) => {
+fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
         } else {
@@ -53,7 +53,7 @@ fs.readFile('./db/db.json', 'utf8', (err, data) => {
             parsedNotes.push(newNote);            // Add the new note
     
             // Write updated notes back to the file
-            fs.writeFile('./db/db.json',
+            fs.writeFile('db/db.json',
             JSON.stringify(parsedNotes, null, 4),
             (writeErr) =>
                 writeErr
@@ -68,7 +68,7 @@ app.delete('/api/notes/:noteID', (req, res) => {
     console.log('Request received to delete note ID: ', req.params.noteID)
     const idToDelete = req.params.noteID;
     // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
         } else {
@@ -78,7 +78,7 @@ app.delete('/api/notes/:noteID', (req, res) => {
             console.log('after', newList);
 
             // Write updated notes to the file
-            fs.writeFile('./db/db.json',
+            fs.writeFile('db/db.json',
             JSON.stringify(newList, null, 4),
             (writeErr) =>
                 writeErr
